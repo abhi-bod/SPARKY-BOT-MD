cf804bbdfe8b30a5c04cc3a80920cbe192067b0f
Abhijith <131227108+abhi-bod@users.noreply.github.com>
2023-04-26 13:05:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,31 +14,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['917012984396'] //ganti nomer lu
-global.ownername = "ASWIN SPARKY" //ganti nama lu
-global.developer = "ASWIN" //name
+global.vcardowner = ['917365952058'] //ganti nomer lu
+global.ownername = "ABHI" //ganti nama lu
+global.developer = "ABHIJTH" //name
 global.ytname = "YT: SPARKY MON 777" //ur yt chanel name
-global.socialm = "https://www.instagram.com/aswin_sparky.exe" //taroh sosialmedia lu kalo ga punya biarin aja
+global.socialm = "https://instagram.com/__abhi_efx?igshid=YmMyMTA2M2Y=" //taroh sosialmedia lu kalo ga punya biarin aja
 global.sgc = "https://chat.whatsapp.com/JTUrdnOziKD44ScoBoggmh" //ganti group lu
 global.location = "Kerala,TVM,Varkala" //ganti lokasi lu
 
 //bot bomdy 
-global.owner = ['917012984396'] //ur number
-global.ownernomer = "917012984396" //ur number
-global.ownertag = '917012984396' //ur tag number
-global.premium = ['917012984396'] //ur premium number
-global.botname = 'SPARKY MD+' //ur bot name
-global.ownername = "ASWIN SPARKY"
+global.owner = ['917365952058'] //ur number
+global.ownernomer = "917365952058" //ur number
+global.ownertag = '917365952058' //ur tag number
+global.premium = ['917365952058'] //ur premium number
+global.botname = 'ABHI-HOD≈BOT+' //ur bot name
+global.ownername = "ABHIJITH"
 global.linkz = "https://youtube.com/@sparkymon777" //your theme url which will be displayed on whatsapp
-global.dana = "917012984396"
-global.pulsa = "917012984396"
+global.dana = "917365952058"
+global.pulsa = "917365952058"
 global.websitex = "https://youtube.com/@sparkymon777" //ur website to be displayed
 global.websitex1 = "https://sparkymodswa.blogspot.com"
 global.botscript = 'https://github.com/Sparkymon777/SPARKY-BOT-MD' //script link
 global.themeemoji = "👀" //ur theme emoji
-global.packname = "S P A R K Y" //ur sticker watermark packname
-global.author = "ASWIN" //ur sticker watermark author
-global.wm = "S P A R K Y" //ur watermark
+global.packname = "ABHI EDIT" //ur sticker watermark packname
+global.author = "ABHI" //ur sticker watermark author
+global.wm = "ABHI EDIT" //ur watermark
 
 // Other
 global.sessionName = 'session'
